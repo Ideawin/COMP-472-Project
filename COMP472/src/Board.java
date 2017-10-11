d4176839e35caaf3f56339b0fe15f25b3faf5534
unknown <lortiz.hannah@gmail.com>
2017-10-11 13:42:34 -0400

    removed black and white comment

--- COMP472/src/Board.java
+++ COMP472/src/Board.java
@@ -2,50 +2,59 @@
 public class Board {
 	final int WIDTH = 9;
 	final int HEIGHT = 5;
-	char[] boardArr;
+	int maxConsecutiveMoves = 10;
+	//char[] boardArr;
+	String[][] boardArr;
 	
+		
 	/**
 	 * Constructor
 	 */
 	public Board() {
 		// Create a new board
-		boardArr = new char[WIDTH*HEIGHT];
+		boardArr = new String[HEIGHT][WIDTH];
+		
+		
+		//black and white positions
+		
+		
+		/*
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				if ((i%2==0) && (j%2==0)) boardArr[i] [j] = "B";
+				else if ((i%2!=0) && (j%2!=0)) boardArr[i] [j] = "B";
+				else  boardArr[i] [j] = "W";
+			}
+		} 
+	 */
+			
 		
 		// Set tokens on the board
-		for (int i = 0; i < boardArr.length; i++) {
-			// A row
-			if (i < WIDTH)
-				boardArr[i] = 'R';
-			// B row
-			else if (i >= WIDTH && i < WIDTH*2)
-			{
-				boardArr[i] = 'R';
+		
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				if (i == 0 || i == 1)
+					boardArr[i] [j] = "R";
+				else if (i == 3 || i == 4)
+					boardArr[i] [j] = "G";
+				else if (i == 2 && j < 4)
+					boardArr[i] [j] = "G";
+				else if (i == 2 && j > 4)
+					boardArr[i] [j] = "R";
+				else boardArr[i] [j] = " ";
 			}
-			// C row
-			else if (i >= WIDTH*2 && i < WIDTH*3) {
-				if (i < WIDTH*2 + 4)
-					boardArr[i] = 'G';
-				else if (i == WIDTH*2 + 4)
-					boardArr[i] = ' ';
-				else
-					boardArr[i] = 'R';
-			}
-			// D row
-			else
-				boardArr[i] = 'G';
 		}
 	}
+
 	
 	/**
 	 * Method to display contents of the board
 	 */
 	public void displayBoard() {
-		for (int i = 0; i < boardArr.length; i++)
-		{
-			if (i%WIDTH == 0 && i != 0) {
-				System.out.println("");
-			}
-			System.out.print(boardArr[i] + " ");
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++)  
+				System.out.print(boardArr[i] [j] + " "); // \t
+			System.out.println();
 		}
 	}
 	
@@ -55,21 +64,37 @@ public class Board {
 	 * @param y
 	 * @param token 'R' or 'G'
 	 */
-	public void moveToken(int currentPos, int oldPos, char token) {
+	/*public void moveToken(int newPos, int oldPos, char token) {
+		// Check if it's your turn
+		// Check if even or odd
+		// Check direction of new position
 		if (boardArr[oldPos] == ' ') {
 			boardArr[oldPos] = token;
-			boardArr[currentPos] = ' ';
+			boardArr[newPos] = ' ';
 		}
-	}
+		// Check if you can attack and how many tokens can be killed
+	}*/
 	
 	/**
-	 * Method to verify if a tile on a board is empty
+	 * Method to verify if a cell is black or white
+	 * @param positionX  positionY
+	 * @return true if cell is black , false is cell is white
+	 */
+	public boolean blackCell(int positionX, int positionY) {
+		 	
+				if ((positionX%2==0) && (positionY%2==0)) return true;  //black cell
+				else if ((positionX%2!=0) && (positionY%2!=0)) return true;  //black cell
+				else  return false;   //white cell
+	}
+	/**
+	 * * Method to verify if a tile on a board is empty
 	 * @param pos
 	 * @return true if empty, false otherwise
+	 
 	 */
-	public boolean isEmpty(int pos) {
+	/*public boolean isEmpty(int pos) {
 		return boardArr[pos] == ' ';
-	}
+	}*/
 	
 	/**
 	 * Method to get the position index of a tile on the board
@@ -77,6 +102,8 @@ public class Board {
 	 * @param x
 	 * @return
 	 */
+	
+	/*
 	public int getPositionIndex(char y, int x) {
 		int arrayNum = -1;
 		if (y >= 'A' && y <= 'E') {
@@ -85,13 +112,15 @@ public class Board {
 		return arrayNum;
 	}
 	
+	*/
+	
 	/**
 	 * Get the token at a certain position
 	 * @param pos
 	 * @return
 	 */
-	public char getTokenAtPosition(int pos) {
+	/*public char getTokenAtPosition(int pos) {
 		return boardArr[pos];
-	}
-	
+	}*/
+
 }
