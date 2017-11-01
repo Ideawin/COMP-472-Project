1099225399f72948c1909d20312e695ace16b995
Philip Lim <lim-philip@hotmail.com>
2017-11-01 15:12:46 -0400

    Added more unit tests for calculateScore()

--- COMP472/src/tests/MiniMaxTest.java
+++ COMP472/src/tests/MiniMaxTest.java
@@ -27,5 +27,89 @@ public class MiniMaxTest {
 		// Assert result
 		assertEquals(0, node.getScore());
 	}
+	
+	@Test
+	public void calculateScoreBoard1() {
+		Node node = new Node(false, 2, null);
+		char[][] mockBoard = new char[Board.HEIGHT][Board.WIDTH];
+		for(int i=0; i<mockBoard.length; i++) {
+			for(int j=0; j<mockBoard[i].length; j++) {
+				mockBoard[i][j] = ' ';
+			}
+		}
+		
+		// Set some mock positions
+		mockBoard[2][4] = 'G';
+		mockBoard[1][6] = 'G';
+		
+		node.setCurrentState(mockBoard);
+		
+		// Create new mini max object
+		MiniMax miniMaxObj = new MiniMax();
+		miniMaxObj.calculateScore(node);
+		
+		// Assert result
+		assertEquals(1100, node.getScore());
+	}
+	
+	@Test
+	public void calculateScoreBoard2() {
+		Node node = new Node(false, 2, null);
+		char[][] mockBoard = new char[Board.HEIGHT][Board.WIDTH];
+		for(int i=0; i<mockBoard.length; i++) {
+			for(int j=0; j<mockBoard[i].length; j++) {
+				mockBoard[i][j] = ' ';
+			}
+		}
+		
+		// Set some mock positions
+		mockBoard[2][0] = 'R';
+		mockBoard[2][1] = 'G';
+		mockBoard[2][2] = 'R';
+		mockBoard[2][3] = 'G';
+		mockBoard[2][5] = 'R';
+		mockBoard[2][6] = 'R';
+		mockBoard[2][7] = 'R';
+		mockBoard[2][8] = 'G';
+		
+		node.setCurrentState(mockBoard);
+		
+		// Create new mini max object
+		MiniMax miniMaxObj = new MiniMax();
+		miniMaxObj.calculateScore(node);
+		
+		// Assert result
+		assertEquals(-1100, node.getScore());
+	}
+	
+	@Test
+	public void calculateScoreBoard3() {
+		Node node = new Node(false, 2, null);
+		char[][] mockBoard = new char[Board.HEIGHT][Board.WIDTH];
+		for(int i=0; i<mockBoard.length; i++) {
+			for(int j=0; j<mockBoard[i].length; j++) {
+				mockBoard[i][j] = ' ';
+			}
+		}
+		
+		// Set some mock positions
+		mockBoard[2][0] = 'R';
+		mockBoard[2][1] = 'G';
+		mockBoard[2][2] = 'R';
+		mockBoard[2][3] = 'G';
+		mockBoard[0][5] = 'R';
+		mockBoard[0][6] = 'R';
+		mockBoard[2][7] = 'R';
+		mockBoard[4][8] = 'G';
+		
+		node.setCurrentState(mockBoard);
+		
+		// Create new mini max object
+		MiniMax miniMaxObj = new MiniMax();
+		miniMaxObj.calculateScore(node);
+		
+		// Assert result
+		assertEquals(-500, node.getScore());
+	}
 
 }
