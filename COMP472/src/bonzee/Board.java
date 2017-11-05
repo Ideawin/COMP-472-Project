ffe06c10da052261a773e90b86d9cffe17ce79d4
limphilip <lim-philip@hotmail.com>
2017-11-05 13:34:05 -0500

    Updated look up level back to 3

--- COMP472/src/bonzee/Board.java
+++ COMP472/src/bonzee/Board.java
@@ -415,7 +415,7 @@ public class Board {
 	 */
 	public void playAI(boolean isGreen) {
 		MiniMax miniMax = new MiniMax();
-		miniMax.makeTree(5, isGreen, boardArr);
+		miniMax.makeTree(MINIMAX_MAX_LEVEL_LOOKUP, isGreen, boardArr);
 		String nextBestMove = miniMax.evaluateChildrenAndGetNextMove();
 		this.moveToken(nextBestMove.charAt(0) - 'A', 
 						nextBestMove.charAt(1) - '0' - 1, 
